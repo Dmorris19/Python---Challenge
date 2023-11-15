0dca9cb642e3cf28977039833f0f0d28e6215a72
Dmorris19 <147549816+Dmorris19@users.noreply.github.com>
2023-11-15 12:09:07 -0500

    Add files via upload

---
+++ OfficialPypoll/Pypoll2.py
@@ -0,0 +1,86 @@
+import csv
+import os
+
+# Read the CSV file
+def read_csv(file_path):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = [row for row in reader]
+    return data
+
+# Analyze the votes
+def analyze_votes(data):
+    # Initialize variables
+    total_votes = 0
+    candidates = {}
+    
+    # Count votes for each candidate
+    for row in data:
+        total_votes += 1
+        candidate = row["Candidate"]
+        if candidate in candidates:
+            candidates[candidate] += 1
+        else:
+            candidates[candidate] = 1
+
+    # Calculate percentages
+    percentages = {candidate: (votes / total_votes) * 100 for candidate, votes in candidates.items()}
+
+    # Find the winner
+    winner = max(percentages, key=percentages.get)
+
+    return total_votes, candidates, percentages, winner
+
+# Print the results
+def print_results(total_votes, candidates, percentages, winner):
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------")
+    
+    for candidate, votes in candidates.items():
+        percentage = percentages[candidate]
+        print(f"{candidate}: {percentage:.3f}% ({votes})")
+
+    print("-------------------------")
+    print(f"Winner: {winner}")
+    print("-------------------------")
+
+# Save the results to a text file
+def save_results_to_file(total_votes, candidates, percentages, winner, output_file="election_results.txt"):
+    with open(output_file, "w") as file:
+        file.write("Election Results\n")
+        file.write("-------------------------\n")
+        file.write(f"Total Votes: {total_votes}\n")
+        file.write("-------------------------\n")
+        
+        for candidate, votes in candidates.items():
+            percentage = percentages[candidate]
+            file.write(f"{candidate}: {percentage:.3f}% ({votes})\n")
+
+        file.write("-------------------------\n")
+        file.write(f"Winner: {winner}\n")
+        file.write("-------------------------\n")
+
+if __name__ == "__main__":
+    # Get the current script directory
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Specify the relative path to the CSV file
+    relative_path = os.path.join("OfficialPyPoll", "resources", "Poll_data.csv")
+
+    # Read and analyze the data
+    data = read_csv(relative_path)
+    total_votes, candidates, percentages, winner = analyze_votes(data)
+
+    # Print and save the results
+    print_results(total_votes, candidates, percentages, winner)
+    save_results_to_file(total_votes, candidates, percentages, winner)
+
+
+ 
+
+
+
+
+
